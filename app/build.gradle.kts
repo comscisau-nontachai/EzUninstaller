1a61595fe3ea88a44c402ccf4b013105ebf66263
nontachai_ <nontachai.j123456789@gmail.com>
2025-10-04 17:03:22 +0700

    update version code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "nontachai.becomedev.uninstaller"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
